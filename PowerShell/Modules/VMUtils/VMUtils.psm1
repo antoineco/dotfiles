93b6f315effea54967ae352ef36a5d4948a4bc4a
Antoine Cotten <hello@acotten.com>
2019-10-22 00:10:52 +0200

    sh: Add PowerShell profile and VMUtils module

---
+++ PowerShell/Modules/VMUtils/VMUtils.psm1
@@ -0,0 +1,35 @@
+﻿<#
+ .Synopsis
+  Get the IP of a VM.
+
+ .Description
+  Returns the first IP address of a running VM. Fails if the VM is not running.
+
+ .Parameter Name
+  Name of the VM.
+
+ .Example
+   # Get first IP address of the VM "Ubuntu".
+   Get-VM-IP Ubuntu
+#>
+function Get-VMIP {
+param (
+    [Parameter(Mandatory=$true)][string]$Name
+)
+
+$vm = Get-VM $Name -ErrorAction stop
+
+if (!$vm.State.Equals([Microsoft.HyperV.PowerShell.VMState]::Running)) {
+    throw "VM $($vm.Name) is not Running"
+}
+
+$addrs = $vm.NetworkAdapters[0].IPAddresses
+
+if (!$addrs) {
+    throw "VM $($vm.Name) has no IP address"
+}
+
+return $addrs[0]
+}
+Set-Alias gvmip Get-VMIP
+Export-ModuleMember -Function Get-VMIP -Alias gvmip
