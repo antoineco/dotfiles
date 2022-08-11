19bc2a222b4f15bbbb3a79faed44876ea34b33d2
Antoine Cotten <hello@acotten.com>
2022-08-12 01:17:58 +0200

    nvim: Add Spring Night color scheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -264,6 +264,9 @@ vim.g.gruvbox_italic = true
 -- Dracula --
 vim.g.dracula_full_special_attrs_support = true
 
+-- Spring Night --
+vim.g.spring_night_high_contrast = 0
+
 -- Apply custom highlights on colorscheme change.
 -- Must be declared before executing ':colorscheme'.
 grpid = vim.api.nvim_create_augroup('custom_highlights_everforest', {})
