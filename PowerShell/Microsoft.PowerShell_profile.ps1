4f69231e9078ec8b6af4b7a59593c5dacf3f2cdb
Antoine Cotten <hello@acotten.com>
2023-03-09 17:17:41 +0100

    sh: Fancy PowerShell prompt

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,15 @@
 # Enable Vi mode
 Set-PSReadlineOption -EditMode Vi -ViModeIndicator Prompt
+
+# Custom mappings (set `Get-PSReadLineKeyHandler`)
+Set-PSReadLineKeyHandler -Chord Ctrl+p -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord Ctrl+n -Function HistorySearchForward
+
+# Prompt
+try {
+    oh-my-posh init pwsh --config $env:POSH_THEMES_PATH\marcduiker.omp.json | Invoke-Expression
+}
+catch {
+    Write-Host 'Failed to invoke oh-my-posh.'
+    Write-Host $_
+}
