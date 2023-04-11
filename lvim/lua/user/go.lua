74e7cb43ab2617df4ebbd3a826451e30248b6693
Antoine Cotten <hello@acotten.com>
2023-04-11 13:37:28 +0200

    nvim: Simplify organizeImports code action invocation

--- lvim/lua/user/go.lua
+++ lvim/lua/user/go.lua
@@ -1,21 +1,12 @@
 local M = {}
 
---- Rearrange Go imports by requesting the "source.organizeImports" code action from gopls.
--- Adapted from https://github.com/golang/tools/blob/master/gopls/doc/vim.md#imports
+-- Rearrange Go imports by requesting the "source.organizeImports" code action from gopls.
+-- Adapted from https://go.googlesource.com/tools/+/refs/tags/v0.8.0/gopls/doc/vim.md#imports
 function M.org_imports(wait_ms)
-  local params = vim.lsp.util.make_range_params()
-  params.context = { only = { 'source.organizeImports'} }
-
-  local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, wait_ms)
-
-  for cid, res in pairs(result or {}) do
-    for _, r in pairs(res.result or {}) do
-      if r.edit then
-        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
-        vim.lsp.util.apply_workspace_edit(r.edit, enc)
-      end
-    end
-  end
+  vim.lsp.buf.code_action({
+    context = { only = { 'source.organizeImports' } },
+    apply = true
+  })
 end
 
 return M
