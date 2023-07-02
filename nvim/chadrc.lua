9e6d25ba821318da7376433126c94169491e8c4d
Antoine Cotten <hello@acotten.com>
2023-07-02 21:47:04 +0200

    nvim: Customize base46 Everforest theme

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -1,9 +1,15 @@
 ---@type ChadrcConfig
 local M = {}
 
+local highlights = require "custom.highlights"
+
 M.ui = {
   theme = "everforest",
   theme_toggle = { "everforest", "everforest_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+  changed_themes = require "custom.themes",
 
   statusline = {
     overriden_modules = function()
---
+++ nvim/highlights.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+---@type Base46HLGroupsList
+M.override = {
+  Comment = { italic = true },
+}
+
+---@type HLTable
+--- All the highlight groups which "base46.integrations" does not include must
+--- be explicitly declared here.
+M.add = {
+  CurSearch = { link = "IncSearch" },
+  ["@constructor.lua"] = { link = "@constructor" },
+  ["@constant.builtin.go"] = { link = "@constant.builtin" },
+  ["@include.go"] = { link = "@include" },
+  ["@namespace.go"] = { link = "@namespace" },
+  DiagnosticUnderlineOk = { sp = "LightGreen", underline = true },
+  DiagnosticUnderlineInformation = { sp = "LightBlue", underline = true },
+  DiagnosticUnderlineHint = { sp = "LightGrey", underline = true },
+  DiagnosticUnderlineWarn = { sp = "Orange", underline = true },
+  DiagnosticUnderlineError = { sp = "Red", underline = true },
+}
+
+return M
