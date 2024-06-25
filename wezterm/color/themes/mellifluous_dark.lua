2c6633fd97a712c0e86f1178eab2d92627e897b2
Antoine Cotten <hello@acotten.com>
2024-06-25 15:30:14 +0200

    term: Darken ANSI color 8 in Mellifluous Dark

--- wezterm/color/themes/mellifluous_dark.lua
+++ wezterm/color/themes/mellifluous_dark.lua
@@ -16,6 +16,7 @@ local M = scheme:new {
 ---@type Colors
 local base = wezterm.color.get_builtin_schemes()['Mellifluous']
 base.background = '#1f1f1f'      -- soft
+base.brights[1] = '#4d4d4d'      -- fg5
 base.selection_bg = '#2f2f2f'    -- bg3
 base.selection_fg = nil
 base.compose_cursor = '#938ca8'  -- ui_blue
