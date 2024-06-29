389a7527e493edda555139c3b39539c279f0a364
Antoine Cotten <hello@acotten.com>
2024-06-29 12:35:27 +0200

    term: Define own Mellifluous Dark colors
    The colors of the theme imported from mbadolato/iTerm2-Color-Schemes are
    slightly inconsistent with the upstream palette.

--- wezterm/color/themes/mellifluous_dark.lua
+++ wezterm/color/themes/mellifluous_dark.lua
@@ -4,22 +4,46 @@ local scheme = require 'color.scheme'
 ---@type Scheme
 local M = scheme:new {
   palette = {
-    bg_dim = '#191919',
-    bg0    = '#282828',
-    bg1    = '#323232',
-    fg     = '#dadada',
-    grey0  = '#848484',
-    grey1  = '#aeaeae'
+    bg_dim = '#191919',  -- dark_bg
+    bg0    = '#282828',  -- bg2
+    bg1    = '#323232',  -- bg4
+    fg     = '#dadada',  -- fg
+    grey0  = '#848484',  -- fg3
+    grey1  = '#aeaeae'   -- fg2
   }
 }
 
 ---@type Colors
-local base = wezterm.color.get_builtin_schemes()['Mellifluous']
-base.background = '#1f1f1f'      -- soft
-base.brights[1] = '#4d4d4d'      -- fg5
-base.selection_bg = '#2f2f2f'    -- bg3
-base.selection_fg = nil
-base.compose_cursor = '#938ca8'  -- ui_blue
+local base = {
+  ansi = {
+    '#323232',  -- bg4
+    '#d59192',  -- red
+    '#b3b393',  -- green
+    '#cbaa88',  -- orange
+    '#a8a1be',  -- blue
+    '#b99bb5',  -- purple
+    '#bfaf8e',  -- yellow
+    '#f1f1f1'   -- fg, lightened(8)
+  },
+  brights = {
+    '#373737',  -- bg5
+    '#f2acad',  -- red, lightened(10)
+    '#cecfae',  -- green, lightened(10)
+    '#e7c5a3',  -- orange, lightened(10)
+    '#c3bcda',  -- blue, lightened(10)
+    '#d5b6d1',  -- purple, lightened(10)
+    '#dbcaa9',  -- yellow, lightened(10)
+    '#fcfcfc'   -- fg, lightened(12)
+  },
+
+  foreground = '#dadada',     -- fg
+  background = '#1f1f1f',     -- bg
+  cursor_fg = '#1f1f1f',      -- bg
+  cursor_bg = '#dadada',      -- fg
+  cursor_border = '#dadada',  -- fg
+  selection_bg = '#2d2d2d',   -- bg3
+  compose_cursor = '#938ca8'  -- ui_blue
+}
 M:build(base)
 
 return M
