c4a100f80dd8bea10ca2f62f71a473b671e7ef13
Antoine Cotten <hello@acotten.com>
2024-07-20 14:41:04 +0200

    nix: Declare system-wide packages
    The main purpose of this 'system' flake is to have a cross-platform way
    of installing the system-wide tools which I typically fetch via APT or
    Homebrew.

---
+++ nix/flakes/system/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "System packages";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      # Expands to an attribute set where each name ('system' argument)
+      # corresponds to an entry in the allSystems list, and each value is
+      # dynamically expanded to the result of applying the function 'f' to an
+      # arbitrary function with an attribute set argument {pkgs}, where pkgs
+      # becomes an attribute set of *all* nixpkgs packages for the evaluated
+      # 'system' argument ('import nixpkgs' evaluates to a lambda).
+      #
+      # Cheat sheet:
+      #
+      #   genAttrs [ "x86_64-linux" "aarch64-darwin" ] (system: "x_" + system )
+      #   => { x86_64-linux = "x_86_64-linux"; aarch64-darwin = "x_aarch64-darwin"; }
+      #
+      #   (f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (system: f { pkgs = [ ("x_" + system) ]; })) ({ pkgs }: { default = pkgs; })
+      #   => { x86_64-linux = { default = [ "x_86_64-linux" ]; }; aarch64-darwin = { default = [ "x_aarch64-darwin" ]; }; }
+      #
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default =
+          pkgs.buildEnv {
+            name = "system-packages";
+            paths = with pkgs; [
+              git
+              gnumake
+              curl
+              jq
+              yq
+              fzf
+              bat
+              ripgrep
+            ];
+          };
+      });
+    };
+}
