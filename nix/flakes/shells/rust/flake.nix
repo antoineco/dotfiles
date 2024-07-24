a6fc8e2b4bcfc6ba66efcda656654b9ea17dda7b
Antoine Cotten <hello@acotten.com>
2024-07-24 09:30:28 +0200

    nix: Add devshell flake for Rust development

---
+++ nix/flakes/shells/rust/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Rust development shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+          ];
+        };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          name = "rust-shell";
+          packages = with pkgs; [
+            rust-bin.stable.latest.default
+          ];
+        };
+      });
+    };
+}
