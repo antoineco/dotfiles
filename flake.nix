55a7754fab92fe58da5527b330786542eb6557a9
Antoine Cotten <hello@acotten.com>
2024-09-05 01:14:18 +0200

    nix: Set stateVersion explicitly on nix-darwin

--- flake.nix
+++ flake.nix
@@ -142,6 +142,11 @@
                 users.acotten = mkUser pkgs // { uid = 501; };
               };
 
+              # Used for backwards compatibility, similarly to NixOS.
+              # Before changing this value read the documentation for this option
+              # (e.g. man configuration.nix or on https://daiderd.com/nix-darwin/manual/).
+              system.stateVersion = 4;
+
               services.nix-daemon.enable = true;
 
               nix = mkNix pkgs;
