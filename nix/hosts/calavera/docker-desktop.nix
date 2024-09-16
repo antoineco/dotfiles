2c42db8e3212a3224ae74d933eb12c3c5a8af520
Antoine Cotten <hello@acotten.com>
2024-09-16 09:49:35 +0200

    nix: Add xdg-utils to support Docker Compose in WSL
    docker/for-win#14266

--- nix/hosts/calavera/docker-desktop.nix
+++ nix/hosts/calavera/docker-desktop.nix
@@ -9,4 +9,10 @@
     { src = "${shadow}/bin/groupadd"; }
     { src = "${shadow}/bin/usermod"; }
   ];
+
+  users.users.acotten.packages = with pkgs; [
+    # Compose links to Docker Desktop by opening 'docker-desktop://' URLs
+    # through xdg-open.
+    xdg-utils
+  ];
 }
