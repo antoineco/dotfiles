d53f9bafd529f3062274ffa3b6e1f9ffe53d7955
Antoine Cotten <hello@acotten.com>
2024-11-28 08:50:09 +0100

    qmk: Default to Mac layout, add toggle for PC layout
    No saving to EEPROM. The key switch is effortless and rarely used.

--- qmk/keyboards/dztech/dz60rgb/keymaps/antoineco/keymap.c
+++ qmk/keyboards/dztech/dz60rgb/keymaps/antoineco/keymap.c
@@ -16,25 +16,35 @@
 
 enum layers {
     _BL,
+    _PL,
     _FL,
     _CL
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: Base Layer (Default Layer)
+  /* Keymap _BL: Base Layer, Mac (Default Layer)
    */
 [_BL] = LAYOUT(
     QK_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   RSFT_T(KC_SLSH),    KC_UP,    KC_DEL,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                       KC_RALT,  MO(_FL),  KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                       KC_RGUI,  MO(_FL),  KC_LEFT,  KC_DOWN,  KC_RGHT
+),
+  /* Keymap _PL: PC Layer
+   */
+[_PL] = LAYOUT(
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+    _______,  KC_LGUI,  KC_LALT,                                _______,                      KC_RALT,  _______,  _______,  _______,  _______
 ),
   /* Keymap _FL: Function Layer
    */
 [_FL] = LAYOUT(
     KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TG(_PL),  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_MPLY,  KC_MPRV,  KC_MNXT,  _______,  _______,  _______,  KC_INS,             KC_PGUP,  _______,
     _______,  _______,  _______,                                TO(_CL),                      _______,  _______,  KC_HOME,  KC_PGDN,  KC_END
@@ -53,6 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool rgb_matrix_indicators_user(void) {
     switch (biton32(layer_state)) {
     case _FL:
+        /* Layer toggle keys
+         */
+        rgb_matrix_set_color(17, RGB_RED); // KC_P
         /* Media keys
          */
         rgb_matrix_set_color(52, RGB_WHITE); // KC_AUDIO_VOL_DOWN
