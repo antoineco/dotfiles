dc702590f261ea70096b233956c3d1cf1818de7a
Antoine Cotten <hello@acotten.com>
2025-03-27 10:16:13 +0100

    nix: Neovim 0.11
    The package is currently only available on the nixos-unstable-small
    branch.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411"; # nixos-24.11
     nixpkgs-unstable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    nixos-unstable-small.url = "nixpkgs/nixos-unstable-small";
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
 
     nixos-wsl = {
@@ -34,6 +35,7 @@
       self,
       nixpkgs,
       nixpkgs-unstable,
+      nixos-unstable-small,
       nixos-wsl,
       nix-darwin,
       determinate,
@@ -145,6 +147,7 @@
 
       darwinConfigurations = {
         colomar = nix-darwin.lib.darwinSystem {
+          specialArgs = { inherit nixos-unstable-small; };
           modules = [ ./nix/hosts/colomar ];
         };
       };
