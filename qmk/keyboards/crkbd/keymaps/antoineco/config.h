7f99ce7c626a5e3eabb14ce67d4fa005ff9a57b9
Antoine Cotten <hello@acotten.com>
2025-05-19 21:34:06 +0200

    qmk: Initial configuration for Corne keyboard (crkbd)
    Includes Vial support[1] to enable experimentation.
    Some features had to be disabled for Vial to be able to fit onto the AVR
    microcontroller(s). This includes switching from RBG Matrix to RBG
    Lighting.
    
    [1]: https://get.vial.today/manual/first-use.html

---
+++ qmk/keyboards/crkbd/keymaps/antoineco/config.h
@@ -0,0 +1,31 @@
+/* Split Keyboard
+ */
+
+#define MASTER_LEFT
+
+/* Tap-Hold
+ */
+
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define CHORDAL_HOLD
+
+/* RGB Lighting
+ */
+
+// Reduce max brightness level to prevent the controller from crashing, as
+// recommended in the keyboard's README.
+#define RGBLIGHT_LIMIT_VAL 120
+
+/* Vial
+ */
+
+#define VIAL_KEYBOARD_UID {0x3B, 0x6B, 0xA0, 0x29, 0x80, 0x56, 0xED, 0xD1}
+
+// Secure unlock combination
+// https://get.vial.today/docs/porting-to-vial.html#6-set-up-a-secure-unlock-combination
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 0
+#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 0
---
+++ qmk/keyboards/crkbd/keymaps/antoineco/keymap.c
@@ -0,0 +1,66 @@
+#include QMK_KEYBOARD_H
+
+/* LAYOUT_split_3x6_3
+┌───┬───┬───┬───┬───┬───┐         ┌───┬───┬───┬───┬───┬───┐
+│   │   │   │   │   │   │         │   │   │   │   │   │   │
+├───┼───┼───┼───┼───┼───┤         ├───┼───┼───┼───┼───┼───┤
+│   │   │   │   │   │   │         │   │   │   │   │   │   │
+├───┼───┼───┼───┼───┼───┤         ├───┼───┼───┼───┼───┼───┤
+│   │   │   │   │   │   │         │   │   │   │   │   │   │
+└───┴───┴───┴───┴───┴───┘         └───┴───┴───┴───┴───┴───┘
+                ┌───┬───┬───┐ ┌───┬───┬───┐
+                │   │   │   │ │   │   │   │
+                └───┴───┴───┘ └───┴───┴───┘
+ */
+
+#define HM_A LCTL_T(KC_A)
+#define HM_S LALT_T(KC_S)
+#define HM_D LGUI_T(KC_D)
+#define HM_F LSFT_T(KC_F)
+
+#define HM_J RSFT_T(KC_J)
+#define HM_K RGUI_T(KC_K)
+#define HM_L LALT_T(KC_L)
+#define HM_SCLN RCTL_T(KC_SCLN)
+
+enum layers {
+    _BL,
+    _NL,
+    _SL,
+    _CL
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Keymap _BL: Base Layer (Default Layer)
+   */
+[_BL] = LAYOUT_split_3x6_3(
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
+    KC_ESC,  HM_A,    HM_S,    HM_D,    HM_F,    KC_G,                        KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_QUOT,
+    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+                                        XXXXXXX, KC_BSPC, TL_UPPR,   TL_LOWR, KC_SPC,  KC_ENT
+),
+  /* Keymap _NL: Numbers and Navigation Layer
+   */
+[_NL] = LAYOUT_split_3x6_3(
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______,
+                                        _______, KC_DEL,  _______,   _______, _______, _______
+),
+  /* Keymap _SL: Symbols Layer
+   */
+[_SL] = LAYOUT_split_3x6_3(
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                     KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
+    KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT,                     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+                                        _______, _______, _______,   _______, _______, _______
+),
+  /* Keymap _CL: Keyboard Control Layer
+   */
+[_CL] = LAYOUT_split_3x6_3(
+    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    UG_TOGG, UG_HUEU, UG_SATU, UG_VALU, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    UG_NEXT, UG_HUED, UG_SATD, UG_VALD, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                        _______, _______, _______,   _______, _______, _______
+)
+};
