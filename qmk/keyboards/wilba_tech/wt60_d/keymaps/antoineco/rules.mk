242cf08f753c00b8549550adece88e7a7c18187c
Antoine Cotten <hello@acotten.com>
2025-05-20 14:50:59 +0200

    qmk: Disable unused features
    https://docs.qmk.fm/config_options#feature-options

--- qmk/keyboards/wilba_tech/wt60_d/keymaps/antoineco/rules.mk
+++ qmk/keyboards/wilba_tech/wt60_d/keymaps/antoineco/rules.mk
@@ -1,2 +1,8 @@
-NKRO_ENABLE = no     # disable N-key rollover
-MOUSEKEY_ENABLE = no # disable support for mouse keys
+NKRO_ENABLE         = no # disable N-key rollover
+MOUSEKEY_ENABLE     = no # disable support for mouse keys
+COMBOS_ENABLE       = no # disable Combos
+TAP_DANCE_ENABLE    = no # disable Tap Dance
+KEY_OVERRIDE_ENABLE = no # disable Key Overrides
+SPACE_CADET_ENABLE  = no # disable Space Cadet Shift
+MAGIC_ENABLE        = no # disable Magic Keycodes
+GRAVE_ESC_ENABLE    = no # disable Grave Escape
