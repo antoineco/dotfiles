74396a927ed5aec639f5380304aa8c98c0cdd0b7
Antoine Cotten <hello@acotten.com>
2025-05-23 18:45:31 +0200

    qmk: Revamp Numbers and Symbols layers
    Numbers on the top row feel uncomfortable to type with the 'lower' thumb
    key pressed. Place them all on the opposite hand like a numpad instead.
    Additionally, place symbols frequently typed in relation with numbers on
    the remaining keys.
    
    For the same reason, symbols on the top row feel uncomfortable to reach
    with the 'upper' thumb key pressed. Spread them across both hands
    instead, taking inspiration from sunaku's symbol layer[1].
    The most frequently hit symbols are placed on the right hand, in a
    Vim-friendly arrangement rather than a mirror of the ANSI layout.
    Most symbols on the left hand are duplicated from the base layer for
    convenience.
    
    Finally, return to the OLKB style "Tri Layer" system[2] for layer keys,
    which was the default on Vial.
    
    [1]: https://sunaku.github.io/moergo-glove80-keyboard.html#symbol-layer
    [2]: https://docs.qmk.fm/features/tri_layer

--- qmk/keyboards/crkbd/keymaps/antoineco/keymap.c
+++ qmk/keyboards/crkbd/keymaps/antoineco/keymap.c
@@ -23,44 +23,37 @@
 #define HM_L LALT_T(KC_L)
 #define HM_SCLN RCTL_T(KC_SCLN)
 
-enum layers {
-    _BL,
-    _NL,
-    _SL,
-    _CL
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: Base Layer (Default Layer)
+  /* Base layer
    */
-[_BL] = LAYOUT_split_3x6_3(
+[0] = LAYOUT_split_3x6_3(
     KC_LPRN, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RPRN,
     KC_ESC,  HM_A,    HM_S,    HM_D,    HM_F,    KC_G,                        KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_QUOT,
-    KC_LCBR, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCBR,
-                                        KC_TAB,  KC_BSPC, MO(_SL),   MO(_NL), KC_SPC,  KC_ENT
+    KC_LBRC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC,
+                                        KC_TAB,  KC_BSPC, TL_UPPR,   TL_LOWR, KC_SPC,  KC_ENT
 ),
-  /* Keymap _NL: Numbers and Navigation Layer
+  /* Lower layer (Numbers, Navigation)
    */
-[_NL] = LAYOUT_split_3x6_3(
-    XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
-    _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
-                                        _______, KC_DEL,  MO(_CL),   _______, _______, _______
+[1] = LAYOUT_split_3x6_3(
+    KC_PERC, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PAST,                     KC_COLN, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, XXXXXXX,
+    KC_PEQL, KC_PMNS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+    KC_EXLM, KC_LT,   KC_P1,   KC_P2,   KC_P3,   KC_GT,                       KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
+                                        KC_PDOT, KC_P0,   _______,   _______, _______, XXXXXXX
 ),
-  /* Keymap _SL: Symbols Layer
+  /* Upper layer (Symbols)
    */
-[_SL] = LAYOUT_split_3x6_3(
-    XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
-    _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                     KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
-    KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT,                     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
-                                        _______, _______, _______,   MO(_CL), _______, _______
+[2] = LAYOUT_split_3x6_3(
+    XXXXXXX, KC_GRV,  KC_AT,   KC_UNDS, KC_DOT,  XXXXXXX,                     XXXXXXX, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, XXXXXXX,
+    KC_TILD, KC_LT,   KC_PIPE, KC_MINS, KC_GT,   KC_SLSH,                     KC_EXLM, KC_COMM, KC_LCBR, KC_RCBR, KC_SCLN, KC_QUES,
+    XXXXXXX, KC_AMPR, KC_QUOT, KC_DQT,  KC_PLUS, KC_BSLS,                     KC_HASH, KC_CIRC, KC_COLN, KC_EQL,  KC_DLR,  KC_ASTR,
+                                        XXXXXXX, _______, _______,   _______, _______, KC_PERC
 ),
-  /* Keymap _CL: Keyboard Control Layer
+  /* Adjust layer (Media, Mouse, Keyboard control)
    */
-[_CL] = LAYOUT_split_3x6_3(
-    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, MS_BTN1, XXXXXXX,                     MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, XXXXXXX, XXXXXXX,
-    RM_NEXT, RM_HUED, RM_SATD, RM_VALD, MS_BTN2, XXXXXXX,                     MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, XXXXXXX, XXXXXXX,
-                                        _______, _______, _______,   _______, _______, _______
+[3] = LAYOUT_split_3x6_3(
+    QK_BOOT, XXXXXXX, RM_TOGG, RM_HUEU, RM_SATU, RM_VALU,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, MS_BTN2, MS_BTN1, XXXXXXX,                     MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, XXXXXXX, XXXXXXX,
+    KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT,                     MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, XXXXXXX, XXXXXXX,
+                                        XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX
 )
 };
