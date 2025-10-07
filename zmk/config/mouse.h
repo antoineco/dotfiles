5a94442e290afe229ad01af9871b6c1cf16fdbdf
Antoine Cotten <hello@acotten.com>
2025-10-07 10:58:44 +0200

    zmk: Declare shared mouse emulation macros

---
+++ zmk/config/mouse.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define ZMK_POINTING_DEFAULT_MOVE_VAL 1500
+#define ZMK_POINTING_DEFAULT_SCRL_VAL 20
+
+#include <dt-bindings/zmk/pointing.h>
+
+#define MMV_LEFT &mmv MOVE_LEFT
+#define MMV_DOWN &mmv MOVE_DOWN
+#define MMV_UP   &mmv MOVE_UP
+#define MMV_RGHT &mmv MOVE_RIGHT
+#define MSC_LEFT &msc SCRL_LEFT
+#define MSC_DOWN &msc SCRL_DOWN
+#define MSC_UP   &msc SCRL_UP
+#define MSC_RGHT &msc SCRL_RIGHT
