c4c29a3a79fe1fc1b2757bfc6367096ab8af0f4b
Antoine Cotten <hello@acotten.com>
2026-03-23 22:35:02 +0100

    nix: macOS hotkey helper
    Modifier masks were harvested from
    https://github.com/jimratliff/GenoMac-user/blob/5fad16a/scripts/settings/symbolichotkeys_helpers.sh

--- nix/hosts/colomar/system.nix
+++ nix/hosts/colomar/system.nix
@@ -39,160 +39,118 @@
         ];
       };
       "com.apple.symbolichotkeys" = {
-        AppleSymbolicHotKeys = {
-          # Input Sources > Select the previous input source
-          "60" = {
-            # Control-Option-Space
-            # Originally Control-Space, which conflicts with my Neovim completion keymap.
-            value = {
-              parameters = [
-                32
-                49
-                786432
-              ];
-              type = "standard";
+        AppleSymbolicHotKeys =
+          let
+            hotkey =
+              mods: key:
+              let
+                modMask = {
+                  shift = 131072; # 2^17
+                  control = 262144; # 2^18
+                  option = 524288; # 2^19
+                  command = 1048576; # 2^20
+                };
+
+                keyCodes = {
+                  # [
+                  #   ASCII
+                  #   AppleScript
+                  # ]
+                  space = [
+                    32
+                    49
+                  ];
+                  c = [
+                    99
+                    8
+                  ];
+                  f = [
+                    102
+                    3
+                  ];
+                  r = [
+                    114
+                    15
+                  ];
+                  left = [
+                    65535
+                    123
+                  ];
+                  right = [
+                    65535
+                    124
+                  ];
+                  down = [
+                    65535
+                    125
+                  ];
+                  up = [
+                    65535
+                    126
+                  ];
+                };
+
+                keycodes =
+                  keyCodes.${lib.toLower key} or [
+                    65535
+                    65535
+                  ];
+
+                modsum = builtins.foldl' (acc: m: acc + (modMask.${lib.toLower m} or 0)) 0 mods;
+              in
+              {
+                value = {
+                  parameters = keycodes ++ [ modsum ];
+                  type = "standard";
+                };
+                enabled = true;
+              };
+          in
+          # Input Sources
+          {
+            # Select the previous input source
+            "60" = hotkey [ "Control" "Option" ] "Space";
+            # Select the next source in Input menu
+            "61" = {
+              # Disabled to free the Control-Option-Space hotkey for
+              # "Select the previous input source" above.
+              enabled = false;
             };
-            enabled = true;
-          };
-          # Input Sources > Select the next source in Input menu
-          "61" = {
-            # Disabled to free the Control-Option-Space hotkey for
-            # "Select the previous input source" above.
-            enabled = false;
-          };
-          # Windows > General > Fill
-          "237" = {
-            value = {
-              parameters = [
-                102
-                3
-                1441792
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > General > Center
-          "238" = {
-            value = {
-              parameters = [
-                99
-                8
-                1441792
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > General > Return to Previous Size
-          "239" = {
-            value = {
-              parameters = [
-                114
-                15
-                1441792
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > Halves > Tile Left Half
-          "240" = {
-            value = {
-              parameters = [
-                65535
-                123
-                9830400
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > Halves > Tile Right Half
-          "241" = {
-            value = {
-              parameters = [
-                65535
-                124
-                9830400
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > Halves > Tile Top Half
-          "242" = {
-            value = {
-              parameters = [
-                65535
-                126
-                9830400
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > Halves > Tile Bottom Half
-          "243" = {
-            value = {
-              parameters = [
-                65535
-                125
-                9830400
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > Arrange > Arrange Left and Right
-          "248" = {
-            value = {
-              parameters = [
-                65535
-                123
-                10354688
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > Arrange > Arrange Right and Left
-          "249" = {
-            value = {
-              parameters = [
-                65535
-                124
-                10354688
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > Arrange > Arrange Top and Bottom
-          "250" = {
-            value = {
-              parameters = [
-                65535
-                126
-                10354688
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-          # Windows > Arrange > Arrange Bottom and Top
-          "251" = {
-            value = {
-              parameters = [
-                65535
-                125
-                10354688
-              ];
-              type = "standard";
-            };
-            enabled = true;
-          };
-        };
+          }
+          # Windows
+          // (
+            # General
+            {
+              # Fill
+              "237" = hotkey [ "Control" "Shift" "Command" ] "F";
+              # Center
+              "238" = hotkey [ "Control" "Shift" "Command" ] "C";
+              # Return to Previous Size
+              "239" = hotkey [ "Control" "Shift" "Command" ] "R";
+            }
+            # Halves
+            // {
+              # Tile Left
+              "240" = hotkey [ "Control" "Shift" "Command" ] "Left";
+              # Tile Right
+              "241" = hotkey [ "Control" "Shift" "Command" ] "Right";
+              # Tile Top
+              "242" = hotkey [ "Control" "Shift" "Command" ] "Up";
+              # Tile Bottom
+              "243" = hotkey [ "Control" "Shift" "Command" ] "Down";
+            }
+            # Arrange
+            // {
+              # Left and Right
+              "248" = hotkey [ "Control" "Option" "Shift" "Command" ] "Left";
+              # Right and Left
+              "249" = hotkey [ "Control" "Option" "Shift" "Command" ] "Right";
+              # Top and Bottom
+              "250" = hotkey [ "Control" "Option" "Shift" "Command" ] "Up";
+              # Bottom and Top
+              "251" = hotkey [ "Control" "Option" "Shift" "Command" ] "Down";
+            }
+          );
       };
     };
   };
