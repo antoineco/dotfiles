antoineco/dotfiles

commit 10ab730592c2f79be565ccca9cb76c4a63750bcd

pkg: Add 'keymap-drawer' to ZMK devShell
nixpkgs needs to be updated due to a build failure on macOS:

  ImportError: dlopen({{storeDir}}/lib/python3.13/site-packages/tree_sitter_devicetree/_binding.cpython-313-darwin.so, 0x0002): Library not loaded: parser
    Referenced from: <F0D5A9F8-FECD-3275-A29C-121AABBC74EC> {{storeDir}}/lib/python3.13/site-packages/tree_sitter_devicetree/_binding.cpython-313-darwin.so
    Reason: tried: 'parser' (no such file), '/System/Volumes/Preboot/Cryptexes/OSparser' (no such file), 'parser' (no such file), '/usr/local/lib/parser' (no such file), '/usr/lib/parser' (no such file, not in dyld cache), '//parser' (no such file), '/System/Volumes/Preboot/Cryptexes/OS//parser' (no such file), '//parser' (no such file), '/usr/local/lib/parser' (no such file), '/usr/lib/parser' (no such file, not in dyld cache)

Because zephyr-nix requires python310, nixpkgs needs to remain below the
revision NixOS/nixpkgs@77ca080475df714d3ea0f2d438eb1494efb208ac for the
time being.

zmk/flake.nix
@@ -58,6 +58,8 @@
               pkgs.cmake
               pkgs.dtc
               pkgs.ninja
+
+              pkgs.keymap-drawer
             ];
           };
         }

commit f13736f1555a914eaf83427d65ead746375c84ea

zmk/nix: Use patch branch for moergo-sc/zmk input
Commit 10ab730592c2f79be565ccca9cb76c4a63750bcd accidentally broke the
Glove80 build, because the 'can' alias for 'python-can' no longer
exists[1] in the updated nixpkgs revision.

[1]: moergo-sc/zmk#41

zmk/flake.nix
@@ -14,7 +14,7 @@
     zephyr-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     # Glove80 ZMK fork with self-contained Zephyr toolchain.
-    glove80-zmk.url = "github:moergo-sc/zmk?ref=v25.11";
+    glove80-zmk.url = "github:moergo-sc/zmk/rename-can"; # v25.1 + python-can patch
     glove80-zmk.flake = false;
 
     zmk-helpers.url = "github:urob/zmk-helpers?ref=v0.3";