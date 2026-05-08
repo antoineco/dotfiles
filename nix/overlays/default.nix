3468b7cbeba2c6f04d557c04108d5eaee0da31a2
Antoine Cotten <hello@acotten.com>
2026-05-08 16:10:29 +0200

    nix: Assign hostPlatform.system to variable

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -4,32 +4,38 @@ inputs.nixpkgs.lib.composeManyExtensions [
   inputs.rust-overlay.overlays.default
   inputs.monolisa.overlays.default
 
-  (final: prev: {
-    inherit (inputs.nixpkgs-unstable.legacyPackages.${final.stdenv.hostPlatform.system})
-      ghostty
+  (
+    final: prev:
+    let
+      inherit (final.stdenv.hostPlatform) system;
+    in
+    {
+      inherit (inputs.nixpkgs-unstable.legacyPackages.${system})
+        ghostty
 
-      go_1_26
-      gopls
-      golangci-lint
-      gofumpt
-      delve
-      gomodifytags
-      gotestsum
-      gotests
-      impl
-      ;
+        go_1_26
+        gopls
+        golangci-lint
+        gofumpt
+        delve
+        gomodifytags
+        gotestsum
+        gotests
+        impl
+        ;
 
-    neovim = final.callPackage ../packages/neovim.nix {
-      pkgs = inputs.nixpkgs-unstable.legacyPackages.${final.stdenv.hostPlatform.system};
-      inherit (inputs) wrappers;
-    };
+      neovim = final.callPackage ../packages/neovim.nix {
+        pkgs = inputs.nixpkgs-unstable.legacyPackages.${system};
+        inherit (inputs) wrappers;
+      };
 
-    polkit_gnome = prev.polkit_gnome.overrideAttrs {
-      # allow xdg-autostart in XDG_CURRENT_DESKTOP=niri
-      postFixup = ''
-        substituteInPlace "$out"/etc/xdg/autostart/polkit-gnome-authentication-agent-1.desktop \
-          --replace-fail "GNOME;" "niri;GNOME;"
-      '';
-    };
-  })
+      polkit_gnome = prev.polkit_gnome.overrideAttrs {
+        # allow xdg-autostart in XDG_CURRENT_DESKTOP=niri
+        postFixup = ''
+          substituteInPlace "$out"/etc/xdg/autostart/polkit-gnome-authentication-agent-1.desktop \
+            --replace-fail "GNOME;" "niri;GNOME;"
+        '';
+      };
+    }
+  )
 ]
