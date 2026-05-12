c60bbcbd63cf049be898f14694e13a8f25187852
Antoine Cotten <hello@acotten.com>
2026-05-12 16:34:22 +0200

    nix: Relocate non host-specific workstation config
    The profile materializes a functional baseline for a Linux desktop with
    Niri as Wayland compositor.
    
    Prior to commit 34beff76e55b2e70110f11099444b4ac256c3c25, the
    'workstation' profile was meant to support both NixOS and macOS
    (nix-darwin) machines, making it unsuitable for Linux desktop options.

--- nix/modules/profiles/workstation.nix
+++ nix/modules/profiles/workstation.nix
@@ -1,15 +1,154 @@
 { pkgs, ... }:
 {
-  users.users.acotten.packages = with pkgs; [
-    neovim
-    jq
-    yq-go
-    bat
-    difftastic
-    ripgrep
-    direnv
-    nix-direnv
-  ];
+  networking.networkmanager.enable = true;
+
+  hardware.bluetooth.enable = true;
+
+  services.tlp.settings = {
+    START_CHARGE_THRESH_BAT0 = 76;
+    STOP_CHARGE_THRESH_BAT0 = 80;
+  };
+
+  boot = {
+    initrd.systemd.enable = true;
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      timeout = 2;
+    };
+  };
+
+  time.timeZone = "Europe/Berlin";
+
+  users.users.acotten = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "video" # brightness control
+    ];
+
+    packages = with pkgs; [
+      neovim
+      jq
+      yq-go
+      bat
+      difftastic
+      ripgrep
+      direnv
+      nix-direnv
+    ];
+  };
 
   environment.pathsToLink = [ "/share/nix-direnv" ];
+
+  environment.systemPackages = with pkgs; [
+    ghostty
+    google-chrome
+    zen-beta
+    brightnessctl
+    adwaita-icon-theme # use nwg-look program to apply
+    polkit_gnome
+    swaynotificationcenter
+    wl-clipboard
+    fuzzel
+  ];
+
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "acotten" ];
+  };
+  environment.etc."1password/custom_allowed_browsers" = {
+    text = ''
+      zen
+    '';
+    mode = "0644";
+  };
+
+  fonts = {
+    packages = with pkgs; [
+      monolisa-plus
+      nerd-fonts.symbols-only
+      adwaita-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+    ];
+
+    fontconfig.defaultFonts = {
+      sansSerif = [ "Adwaita Sans" ];
+      monospace = [ "Adwaita Mono" ];
+    };
+  };
+
+  programs.niri.enable = true;
+
+  programs.waybar.enable = true;
+
+  services.greetd = {
+    enable = true;
+    useTextGreeter = true;
+    settings.default_session.command =
+      let
+        # The 'niri-session' startup script imports the entirety of the login
+        # manager's environment into systemd, so that it becomes available to
+        # the niri.service unit (and other user services).
+        # Not only calling 'systemctl import-environment' without argument is
+        # deprecated, but most of the login manager's environment is useless.
+        # In fact, some of the environment variables initialized by Bash inside
+        # niri-session can even become problematic when exported to the
+        # environment of the Wayland session (SHLVL, SHELL, TERM, PWD).
+        #
+        # Refs.
+        #   niri-wm/niri#254
+        #   niri-wm/niri#3734
+        niriSessionPatched =
+          pkgs.runCommand "patched-niri-session"
+            {
+              nativeBuildInputs = [ pkgs.patch ];
+            }
+            ''
+              cp ${pkgs.niri}/bin/niri-session $out
+              patch $out < ${./niri-systemd-no-import-env.patch}
+            '';
+      in
+      "${pkgs.greetd}/bin/agreety --cmd '${niriSessionPatched} -l'";
+  };
+
+  systemd.packages = with pkgs; [
+    swaynotificationcenter
+  ];
+  services.dbus.packages = [
+    pkgs.swaynotificationcenter
+  ];
+
+  systemd.user.services.swaync.wantedBy = [ "graphical-session.target" ];
+
+  systemd.user.services.swaybg = {
+    description = "Wallpaper tool for Wayland compositors.";
+    documentation = [ "https://github.com/swaywm/swaybg" ];
+    partOf = [ "graphical-session.target" ];
+    requires = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    unitConfig = {
+      ConditionEnvironment = "WAYLAND_DISPLAY";
+    };
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.swaybg}/bin/swaybg -i %h/wall.jpg -m fill";
+      Restart = "on-failure";
+    };
+    wantedBy = [ "graphical-session.target" ];
+  };
+
+  # Registers the GNOME Keyring and gcr D-Bus services.
+  # Additionally enables the gcr-ssh-agent user service and the integration between the greetd PAM service and gnome-keyring.
+  services.gnome.gnome-keyring.enable = true;
+
+  # Registers the Seahorse D-Bus service and sets SSH_ASKPASS to Seahorse's prompt.
+  programs.seahorse.enable = true;
+
+  # Enables the pam_fprintd module (fprintAuth) for all registered PAM services (security.pam.services.<name>).
+  services.fprintd.enable = true;
+  # The password must be entered for gnome-keyring to be auto-unlocked.
+  security.pam.services.greetd.fprintAuth = false;
 }
