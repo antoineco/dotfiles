9b0c1b05c7b735d77cc55f48395583b3e10a08fe
Antoine Cotten <hello@acotten.com>
2026-06-29 10:57:36 +0200

    nix: Remove unnecessary forAllSystems lambda
    This flake is only ever used on a x86_64-linux host.

--- zmk/flake.nix
+++ zmk/flake.nix
@@ -29,67 +29,50 @@
       zmk-helpers,
       ...
     }:
-    let
-      allSystems = [
-        "x86_64-linux"
-      ];
-      forAllSystems =
-        f:
-        nixpkgs.lib.genAttrs allSystems (
-          system:
-          f {
-            pkgs = import nixpkgs { inherit system; };
-          }
-        );
-    in
     {
-      devShells = forAllSystems (
-        { pkgs }:
-        let
-          zephyr = zephyr-nix.packages.${pkgs.stdenv.hostPlatform.system};
-        in
-        {
-          default = pkgs.mkShellNoCC {
-            packages = [
-              zephyr.pythonEnv
-              (zephyr.sdk-0_16.override { targets = [ "arm-zephyr-eabi" ]; })
+      devShells.x86_64-linux = {
+        default =
+          let
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            zephyrPkgs = zephyr-nix.packages.x86_64-linux;
+          in
+          pkgs.mkShellNoCC {
+            packages =
+              (with pkgs; [
+                cmake
+                dtc
+                ninja
 
-              pkgs.cmake
-              pkgs.dtc
-              pkgs.ninja
-
-              pkgs.keymap-drawer
-            ];
+                keymap-drawer
+              ])
+              ++ (with zephyrPkgs; [
+                pythonEnv
+                (sdk-0_16.override { targets = [ "arm-zephyr-eabi" ]; })
+              ]);
           };
-        }
-      );
+      };
 
-      packages = forAllSystems (
-        { pkgs }:
-        {
-          glove80 =
-            let
-              firmware = import glove80-zmk {
-                # Use moergo's pinned nixpkgs, but set 'system' explicitly because
-                # 'builtins.currentSystem' is disabled in pure mode (the default).
-                # https://github.com/moergo-sc/zmk/blob/v25.11/nix/pinned-nixpkgs.nix#L1
-                pkgs = import (glove80-zmk + /nix/pinned-nixpkgs.nix) {
-                  inherit (pkgs.stdenv.hostPlatform) system;
-                };
-              };
+      packages.x86_64-linux = {
+        glove80 =
+          let
+            # Use moergo's pinned nixpkgs, but set 'system' explicitly because
+            # 'builtins.currentSystem' is disabled in pure mode (the default).
+            # https://github.com/moergo-sc/zmk/blob/v25.11/nix/pinned-nixpkgs.nix#L1
+            firmware = import glove80-zmk {
+              pkgs = import (glove80-zmk + /nix/pinned-nixpkgs.nix) { system = "x86_64-linux"; };
+            };
 
-              config = ./config;
-              overrides = {
-                keymap = "${config}/glove80.keymap";
-                kconfig = "${config}/glove80.conf";
-                extraModules = [ zmk-helpers ];
-              };
+            config = ./config;
+            overrides = {
+              keymap = "${config}/glove80.keymap";
+              kconfig = "${config}/glove80.conf";
+              extraModules = [ zmk-helpers ];
+            };
 
-              left = firmware.zmk.override (overrides // { board = "glove80_lh"; });
-              right = firmware.zmk.override (overrides // { board = "glove80_rh"; });
-            in
-            firmware.combine_uf2 left right;
-        }
-      );
+            left = firmware.zmk.override (overrides // { board = "glove80_lh"; });
+            right = firmware.zmk.override (overrides // { board = "glove80_rh"; });
+          in
+          firmware.combine_uf2 left right;
+      };
     };
 }
